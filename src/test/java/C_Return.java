e97ed1c090fbff6b9c480dc231428ba7378153e3
vigneshwaran369 <nvigneshwaran88@gmail.com>
2023-03-27 18:40:39 +0530

    added

---
+++ src/test/java/C_Return.java
@@ -0,0 +1,22 @@
+
+public class C_Return {
+
+	 int c ;
+
+	public int v() {
+		 return c;
+	}
+
+	private void vicky() {
+
+	}
+	
+	
+	public static void main(String[] args) {
+C_Return s = new C_Return();
+//s.v();
+s.c();
+System.out.println(c);
+	}
+
+}
